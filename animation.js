3c59859b8b4123e0da091393f1e263d912dc5f70
Vladimir Leino <v.leino@ya.ru>
2014-05-13 17:04:28 +0400

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,53 +1,39 @@
 $(document).ready(function() {
 var pi =  Math.PI;
-var s = canvas.width,
-    d = canvas.height;
+var s = Math.floor(Math.random() * window.innerWidth),
+    d = Math.floor(Math.random() * window.innerHeight);
 
 $( document ).on( "mousemove", function( event ) {
   s = event.pageX;
   d = event.pageY;
-
-// console.log( "pageX: " + event.pageX + ", pageY: " + event.pageY );
 });
 
     (function() {
         var canvas = document.getElementById('canvas'),
             context = canvas.getContext('2d');
 
-        // resize the canvas to fill browser window dynamically
         window.addEventListener('resize', resizeCanvas, false);
         window.addEventListener('mousemove', drawStuff, false);
 
-        // window.addEventListener('scroll', resizeCanvas, false);
         function resizeCanvas() {
 
                 canvas.width = window.innerWidth;
                 canvas.height = window.innerHeight;
 
-                /**
-                 * Your drawings need to be inside this function otherwise they will be reset when
-                 * you resize the browser window and the canvas goes will be cleared.
-                 */
-
                 drawStuff();
         }
+
         resizeCanvas();
 
         function drawStuff() {
 
-
-
             var x = new Array(10);
             var y = new Array(10);
-                context.save();
 
-                // Use the identity matrix while clearing the canvas
+                context.save();
                 context.setTransform(1, 0, 0, 1, 0, 0);
                 context.clearRect(0, 0, canvas.width, canvas.height);
-
-                // Restore the transform
                 context.restore();
-
 
             context.beginPath();
 
@@ -60,9 +46,6 @@ $( document ).on( "mousemove", function( event ) {
                          h = Math.floor(Math.random() * $(document).height());
                          context.lineTo(g,h);
                          context.lineWidth = 1;
-                         var b;
-                         // console.log(b +' '+Math.pow(b, 2));
-
                          x[i] = a;
                          y[i] = b;
                          }
@@ -72,13 +55,5 @@ $( document ).on( "mousemove", function( event ) {
             context.stroke();
 
         }
-
-
     })();
-
-});
-
-
-
-
 });
